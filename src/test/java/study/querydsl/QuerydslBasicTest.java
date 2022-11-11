0675c84107231567037cc140ca125b5cfa34a863
Fouink <yyss9103@gmail.com>
2022-11-11 17:52:36 +0900

    QueryDSL 정렬 및 다양한 상황 TestCase 추가

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -1,5 +1,6 @@
 package study.querydsl;
 
+import com.querydsl.core.QueryResults;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,6 +17,7 @@ import javax.persistence.EntityManager;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
+import static study.querydsl.entity.QMember.*;
 
 @SpringBootTest
 @Transactional
@@ -59,12 +61,97 @@ public class QuerydslBasicTest {
     public void startQueryDSL() {
         // given
         //JPQL factory는 필드레벨로 뺌
-        QMember member = new QMember("m");
+        //QMember qMember = QMember.member;
+        //QMember m1 = new Qmember("m1")    -> 같은 테이블을 조인하는 경우에는 선언. 그렇지 않은 경우는 딱히 필요 없음
+        //Qmember는 스태틱으로 뺌
 
         // when
-        Member findMember = queryFactory.select(member).from(member).where(member.username.eq("member1")).fetchOne();
+        Member findMember = queryFactory
+                .select(member)
+                .from(member)
+                .where(member.username.eq("member1"))
+                .fetchOne();
 
         // then
         assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    @DisplayName("검색쿼리")
+    public void search() {
+        // when
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .where(member.username.eq("member1")
+                        .and(member.age.eq(10)))
+                .fetchOne();
+
+        // then
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+        assertThat(findMember.getAge()).isEqualTo(10);
+    }
+
+    @Test
+    @DisplayName("검색쿼리")
+    public void searchAndParam() {
+        // when
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .where(
+                        member.username.eq("member1"),
+                        member.age.eq(10)
+                )                                   //-> 동적쿼리 만들 때 유용
+                .fetchOne();
+
+        // then
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+        assertThat(findMember.getAge()).isEqualTo(10);
+    }
+
+    @Test
+    @DisplayName("QueryDSL 종류")
+    public void resultFetch() {
+//        List<Member> fetch = queryFactory.selectFrom(member).fetch();
+//
+//        Member fetchOne = queryFactory.selectFrom(member).where(member.username.eq("member1")).fetchOne();
+//
+//        Member fetchFirst = queryFactory.selectFrom(member).fetchFirst();
+
+        QueryResults<Member> results = queryFactory.selectFrom(member).fetchResults();
+
+        results.getTotal();
+        List<Member> content = results.getResults();
+    }
+
+    /**
+     * 회원 정렬 순서
+     * 1. 회원 나이 내림차순
+     * 2. 회원 이름 오름차순
+     * 단 2에서 회원 이름이 없으면 마지막에 출력 (nulls last)
+     */
+    @Test
+    @DisplayName("QueryDSL 으로 정렬")
+    public void sort() {
+        // given
+        em.persist(new Member(null, 100));
+        em.persist(new Member("member5", 100));
+        em.persist(new Member("member6", 100));
+
+        // when
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .where(member.age.eq(100))
+                .orderBy(member.age.desc(), member.username.asc().nullsLast())
+                .fetch();
+
+        // then
+        Member member5 = result.get(0);
+        Member member6 = result.get(1);
+        Member memberNull = result.get(2);
+
+        assertThat(member5.getUsername()).isEqualTo("member5");
+        assertThat(member6.getUsername()).isEqualTo("member6");
+        assertThat(memberNull.getUsername()).isNull();
+
     }
 }
